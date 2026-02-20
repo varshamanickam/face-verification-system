0193d4310adeda7536d671cad818c881b60b7175
varshamanickam <118304938+varshamanickam@users.noreply.github.com>
2026-02-20 11:48:16 -0600

    Added similarity metrics functino stubs

---
+++ src/similarity_metrics.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def euclidean_distance_loop(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+    """
+    """
+
+def euclidean_distance_vector(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+    """
+    """
+
+def cosine_similarity_loop(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+    """
+    """
+def cosine_similarity_vector(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+    """
+    """
